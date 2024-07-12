b13e0696868fcf64f9ccb800fb260118ec705ff0
Sierra <sberkemeier15@gmail.com>
2024-07-11 20:03:42 -0400

    update on Skoolie Quest Navbar

---
+++ javascript.js
@@ -0,0 +1,28 @@
+// Mobile View Toggle in Navbar
+<script>
+    document.getElementById('nav-toggle').addEventListener('click', function () {
+        var navLinks = document.getElementById('nav-links');
+        if (navLinks.style.display === 'flex') {
+            navLinks.style.display = 'none';
+        } else {
+            navLinks.style.display = 'flex';
+        }
+    });
+</script>
+
+//Jumbotron
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
